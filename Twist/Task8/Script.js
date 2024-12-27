aabfd6793143875951d5b994d2f7c30b1adb315d
ferasmarjieh <ferasmarjieh@mail.tau.ac.il>
2024-12-27 19:54:34 +0200

    task8

---
+++ Twist/Task8/Script.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+console.log("Script loaded successfully!");
+
+
+function divClicked() {
+    const dorothy = document.getElementById("dorothy");
+    dorothy.style.left = "300px";
+    dorothy.style.top = "400px";
+}
